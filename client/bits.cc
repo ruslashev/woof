230714908b71e7b89ec02bbad1fe4280e06629ef
Ruslan Akbashev <ruslashev@users.noreply.github.com>
2016-12-17 21:07:19 +0300

    Update bits.cc

--- client/bits.cc
+++ client/bits.cc
@@ -19,10 +19,10 @@ void bytestream::clear() {
   _index = 0;
 }
 
-void bytestream::write_uint32(uint32_t value) {
-  _data.resize(_data.size() + 4);
-  *((uint32_t*)(_data.data() + _index)) = htonl(value);
-  _index += 4;
+void bytestream::write_uint8(uint8_t value) {
+  _data.resize(_data.size() + 1);
+  *(_data.data() + _index) = value;
+  _index += 1;
 }
 
 void bytestream::write_uint16(uint16_t value) {
@@ -31,10 +31,37 @@ void bytestream::write_uint16(uint16_t value) {
   _index += 2;
 }
 
-void bytestream::write_uint8(uint8_t value) {
-  _data.resize(_data.size() + 1);
-  *(_data.data() + _index) = value;
-  _index += 1;
+void bytestream::write_uint32(uint32_t value) {
+  _data.resize(_data.size() + 4);
+  *((uint32_t*)(_data.data() + _index)) = htonl(value);
+  _index += 4;
+}
+
+bool bytestream::read_uint8(uint8_t &value) {
+  if (_rindex + 1 <= _data.size()) {
+    value = *((uint8_t*)(_data.data() + _rindex));
+    _rindex += 1;
+    return true;
+  } else
+    return false;
+}
+
+bool bytestream::read_uint16(uint16_t &value) {
+  if (_rindex + 2 <= _data.size()) {
+    value = *((uint16_t*)(_data.data() + _rindex));
+    _rindex += 2;
+    return true;
+  } else
+    return false;
+}
+
+bool bytestream::read_uint32(uint32_t &value) {
+  if (_rindex + 4 <= _data.size()) {
+    value = *((uint32_t*)(_data.data() + _rindex));
+    _rindex += 4;
+    return true;
+  } else
+    return false;
 }
 
 void bytestream::append(const bytestream &b) {
