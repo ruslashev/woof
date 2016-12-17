0e3b2c2ea671765f97f8c8672c2113f60fed2626
Ruslan Akbashev <ruslashev@users.noreply.github.com>
2016-12-17 21:05:56 +0300

    Update bits.hh

--- client/bits.hh
+++ client/bits.hh
@@ -15,8 +15,10 @@ public:
   void write_uint8(uint8_t value);
   void write_uint16(uint16_t value);
   void write_uint32(uint32_t value);
+  bool read_uint8(uint8_t &value);
+  bool read_uint16(uint16_t &value);
+  bool read_uint32(uint32_t &value);
   void append(const bytestream &b);
   void print(const char *msg = "bytestream");
   bytestream();
 };
-
