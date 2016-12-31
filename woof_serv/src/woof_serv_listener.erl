d655d58e8d5611c707ef6aa2e6fa2c35c061374e
Ruslan Akbashev <ruslashev@gmail.com>
2016-12-31 18:44:35 +0300

    woof_serv_listener: rollback to gen_server

--- woof_serv/src/woof_serv_listener.erl
+++ woof_serv/src/woof_serv_listener.erl
@@ -1,32 +1,44 @@
 -module(woof_serv_listener).
--export([start_link/0, init/1]).
+-behaviour(gen_server).
+-export([start_link/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+         code_change/3]).
 
--define(ACTIVE, { active, once }).
+-define(ACTIVE, { active, true }).
 -define(SOCK_OPTS, [binary, ?ACTIVE]).
 
 start_link() ->
-    spawn_link({ local, woof_serv_listener }, ?MODULE, init, []).
+    gen_server:start_link({ local, woof_serv_listener }, ?MODULE, [], []).
 
 init([]) ->
     { ok, PortServer } = application:get_env(port_serv),
-    case gen_udp:open(PortServer, ?SOCK_OPTS) of
+    case gen_udp:open(PortServer, [binary, ?ACTIVE]) of
         { ok, Socket } ->
-            loop(Socket);
+            { ok, Socket };
         { error, Reason } ->
-            io:format("Failed to open socket: ~p~n", [Reason]),
-            exit(Reason)
+            io:format("woof_serv_listener: failed to open socket: ~p~n", [Reason]),
+            { stop, Reason }
     end.
 
-loop(Socket) ->
-    inet:setopts(Socket, ?SOCK_OPTS),
-    receive
-        Message = { udp, _, _, _, _ } ->
-            woof_serv_handler_sup:handle(Message);
-        terminate ->
-            gen_udp:close(Socket),
-            exit(normal);
-        Unknown ->
-            io:format("woof_serv_listener: unknown info: ~p~n", [Unknown])
-    end,
-    loop(Socket).
+handle_info(Message = { udp, _, _, _, _ }, Socket) ->
+    woof_serv_handler_sup:handle(Message),
+    { noreply, Socket };
+handle_info(Unknown, State) ->
+    io:format("woof_serv_listener: unknown info: ~p~n", [Unknown]),
+    { noreply, State }.
+
+handle_call(Unknown, _From, State) ->
+    io:format("woof_serv_listener: unknown call: ~p~n", [Unknown]),
+    { noreply, State }.
+
+handle_cast(Unknown, State) ->
+    io:format("woof_serv_listener: unknown cast: ~p~n", [Unknown]),
+    { noreply, State }.
+
+code_change(_OldVsn, State, _Extra) ->
+    { ok, State }.
+
+terminate(Reason, Socket) ->
+    io:format("woof_serv_listener: terminate: ~p~n", [Reason]),
+    gen_udp:close(Socket).
 
