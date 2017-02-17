3a032168e3015c2fa8dfdf773d8b201de829916b
Ruslan Akbashev <ruslashev@gmail.com>
2017-02-17 15:10:51 +0300

    woof_packet: implement append_msg_queue/2

--- woof_serv/src/woof_packet.erl
+++ woof_serv/src/woof_packet.erl
@@ -1,5 +1,5 @@
 -module(woof_packet).
--export([serialize/1]).
+-export([serialize/1, append_msg_queue/2]).
 -include("woof_common.hrl").
 
 serialize(#packet{
@@ -12,7 +12,14 @@ serialize(#packet{
     <<Reliable:8/big, Sequence:32/big, Ack:32/big, ClientId:16/big,
       NumMessages:8/big, SerializedMessages/big>>.
 
-append_msg_queue(MsgQueue, #packet{
+append_msg_queue(MsgQueue, Packet = #packet{
         serialized_messages = SerializedMessages }) ->
-    #packet { serialized_messages = }
+    case queue:out(MsgQueue) of
+        { empty, _ } ->
+            Packet;
+        { { value, HeadMsg }, NewQueue } ->
+            NewPacket = #packet{ serialized_messages =
+                    <<SerializedMessages/binary, HeadMsg/binary>> },
+            append_msg_queue(NewQueue, NewPacket)
+    end.
 
